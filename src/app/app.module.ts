46145a27c1454b033425f5051a5dc03457853196
Kevin Shemili <kevinshemili5@gmail.com>
2022-08-29 16:25:01 +0200

    create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { LoginformComponent } from './_components/loginform/loginform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginformComponent,
   ],
   imports: [
     BrowserModule,
